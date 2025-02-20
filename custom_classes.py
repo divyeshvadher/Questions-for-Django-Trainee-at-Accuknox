b487763a4bffc19d350a6f14fd0bd57e6c933b78
divyeshvadher <divyeshvadher782@gmail.com>
2025-02-20 12:40:51 +0530

    Add Custom Classes in python solution

---
+++ custom_classes.py
@@ -0,0 +1,21 @@
+class Rectangle:
+    def __init__(self, length: int, width: int):
+        self.length = length
+        self.width = width
+
+    def __iter__(self):
+        yield {"length": self.length}
+        yield {"width": self.width}
+
+# Example usage
+rect = Rectangle(10, 5)
+
+# Iterating over the instance
+for attr in rect:
+    print(attr)
+
+
+# Expected Output
+
+# {'length': 10}
+# {'width': 5}
